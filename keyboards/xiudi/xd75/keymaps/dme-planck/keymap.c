e5385c7c1625ae5d26097589bcf251f430be46b6
David Edmondson <dme@dme.org>
2023-11-08 10:52:13 +0000

    xd75/dme-planck: Add colemak-dh

--- keyboards/xiudi/xd75/keymaps/dme-planck/keymap.c
+++ keyboards/xiudi/xd75/keymaps/dme-planck/keymap.c
@@ -18,6 +18,7 @@
 
 enum dme_planck_layers {
   _QWERTY,
+  _COLEMAK,
   _LOWER,
   _RAISE,
   _ADJUST
@@ -42,11 +43,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT_ortho_5x15(
-				_______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-				KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, _______, _______, _______,
-				KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, _______, _______, _______,
-				KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , _______, _______, _______,
-				KC_ESC, KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______
+			        KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, _______, _______, _______,
+				KC_TAB,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, _______, _______, _______,
+				LCTL_T(KC_ESC), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  _______, _______,
+				KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, _______, _______,
+				RAISE,          KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC, KC_SPC,   KC_BSPC, _______, _______, _______, _______, DF(_COLEMAK), _______, _______
+				),
+
+
+
+
+  [_COLEMAK] = LAYOUT_ortho_5x15(
+			        KC_GRV,         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC, _______, _______, _______,
+				KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, _______, _______, _______,
+				LCTL_T(KC_ESC), KC_A,    KC_R,    KC_S,    KC_T,    KC_G,    KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, KC_ENT,  _______, _______,
+				KC_LSFT,        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, _______, _______, _______,
+				RAISE,          KC_LCTL, KC_LGUI, KC_LALT, LOWER,   KC_SPC, KC_SPC,   KC_BSPC, _______, _______, _______, _______, DF(_QWERTY), _______, _______
 				),
 
   /* Lower
